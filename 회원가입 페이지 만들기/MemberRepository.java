04b1da54e17b149b0a0d5922199ced69789dafe0
Sungwoong Park <42471370+sungw00ng@users.noreply.github.com>
2025-06-08 23:14:04 +0900

    Delete 회원가입 페이지 만들기 directory

--- 회원가입 페이지 만들기/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.fristproject.repository;
-
-import com.example.fristproject.entity.Member;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MemberRepository extends CrudRepository<Member,Long> {
-
-}
